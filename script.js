2846972ac28cafe1deeed73c031e0e3fff167e8e
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2025-01-30 22:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
         div.addEventListener("dragstart", function (event) {
             draggedElement = this;
             event.dataTransfer.setData("text/plain", this.id);
-            setTimeout(() => this.classList.add("dragging"), 0);
+            setTimeout(() => this.classList.add("drag"), 0);
         });
 
         div.addEventListener("dragend", function () {
-            this.classList.remove("dragging");
+            this.classList.remove("drag");
         });
 
         div.addEventListener("dragover", function (event) {
             event.preventDefault();
-            const dragging = document.querySelector(".dragging");
+            const dragging = document.querySelector(".drag");
             const parent = this.parentNode;
             const siblings = [...parent.children].filter(child => child !== dragging);
             let nextSibling = siblings.find(sibling => event.clientX < sibling.getBoundingClientRect().left + sibling.offsetWidth / 2);
